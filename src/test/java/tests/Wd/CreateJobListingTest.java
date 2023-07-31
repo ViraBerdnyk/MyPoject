ab5d12d9e09357f344081544573609b5096b6a92
ViraBerdnyk <131290413+ViraBerdnyk@users.noreply.github.com>
2023-07-31 17:29:23 +0200

    Job

--- src/test/java/tests/Wd/CreateJobListingTest.java
+++ src/test/java/tests/Wd/CreateJobListingTest.java
@@ -1,8 +1,9 @@
 package tests.Wd;
 
-
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import my.project.Pages.EditUserProfilePopup;
+import com.codeborne.selenide.Selenide;
+import my.project.Pages.AddJobPage;
 import my.project.Pages.LoginPage;
 import my.project.Pages.ProfilePage;
 import my.project.Pages.WelcomePage;
@@ -12,31 +13,35 @@ import org.testng.annotations.Test;
 import tests.listeners.CustomExtentReportsListener;
 
 import static com.codeborne.selenide.Selenide.open;
-import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 @Listeners({CustomExtentReportsListener.class})
-public class ProfileTest {
+public class CreateJobListingTest {
+
     @BeforeClass
     public void setUp() {
         Configuration.browserSize = "1920x1080";
     }
 
     @Test
-    public void profileTest() {
+    public void createJobListingTest() {
         WelcomePage welcomePage = open("https://freelance.lsrv.in.ua/profile", WelcomePage.class);
         LoginPage loginPage = welcomePage.clickLogInButton();
-        loginPage.setUserName("Tom")
+        ProfilePage profilePage = loginPage.setUserName("Tom")
                 .setPassword("12345678")
                 .clickOnLogInButton();
 
-        ProfilePage profilePage = new ProfilePage();
         profilePage.clickOnProfileDropdownButton();
         profilePage.clickOnProfileMenuItem();
-        EditUserProfilePopup editPopup = profilePage.clickOnEditInfo();
-        editPopup.setName("John")
-                .setLastname("Doe")
-                .clickOnUpdateButton();
 
-        assertEquals(profilePage.getUserFullName(), "John Doe");
+        // Create a new job listing
+        AddJobPage addJobPage = new AddJobPage();
+        String jobTitle = "New Job Listing";
+        String jobDescription = "This is a test job listing.";
+        double jobPrice = 100.0;
+        profilePage = addJobPage.addNewJob(jobTitle, jobDescription, jobPrice);
+
+        // Verify if it is displayed in the list of user's jobs
+        assertTrue(profilePage.isJobDisplayedInMyJobs(jobTitle));
     }
 }
